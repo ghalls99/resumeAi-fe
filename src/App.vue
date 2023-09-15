d0c4d4e8e72f175aae59019a29a3bc6a19f23b1c
ghalls99 <garrett@hallsnet.com>
2023-09-15 12:36:55 -0600

    remaining changes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<CreateAccount msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+	import CreateAccount from './components/CreateAccount.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			CreateAccount,
+		},
+	};
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+</style>
